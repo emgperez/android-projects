74c11454e0e10cb5aef59a9d70ffbfed61882e7b
emgperez <em.gonzalez.perez@gmail.com>
2018-01-14 21:48:53 +0100

    Initial commit for the Quiz App

---
+++ FriendsTVShowBasicQuiz/app/src/main/java/com/example/android/friendstvshow_basicquiz/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.friendstvshow_basicquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
