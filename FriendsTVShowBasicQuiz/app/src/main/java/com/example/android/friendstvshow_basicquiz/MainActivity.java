cd502d4e49e03a07c2bfda9bad02661b8a2bd346
emgperez <em.gonzalez.perez@gmail.com>
2018-01-15 21:18:20 +0100

    Added initial logic to Java code to check how many answers were correct

--- FriendsTVShowBasicQuiz/app/src/main/java/com/example/android/friendstvshow_basicquiz/MainActivity.java
+++ FriendsTVShowBasicQuiz/app/src/main/java/com/example/android/friendstvshow_basicquiz/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.android.friendstvshow_basicquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    int correctAnswers = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void checkAnswers(View view) {
+
+        // Get questions answers
+
+        // Question number one
+        RadioButton answerOne = (RadioButton)findViewById(R.id.choice_1_1);
+        if (answerOne.isChecked())
+            correctAnswers += 1;
+
+        // Question number two
+        RadioButton answerTwo = (RadioButton)findViewById(R.id.choice_2_3);
+        if (answerTwo.isChecked())
+            correctAnswers += 1;
+
+        // Question number three
+        RadioButton answerThree = (RadioButton)findViewById(R.id.choice_3_2);
+        if (answerThree.isChecked())
+            correctAnswers += 1;
+
+        // Question number four
+        RadioButton answerFour = (RadioButton)findViewById(R.id.choice_4_1);
+        if (answerFour.isChecked())
+            correctAnswers += 1;    
+
+
+
+
     }
 }
