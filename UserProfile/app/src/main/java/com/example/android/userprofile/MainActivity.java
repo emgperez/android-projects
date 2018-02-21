e03f0bb625d5d12e1638aaf9d76dfbca4311e657
emgperez <em.gonzalez.perez@gmail.com>
2018-02-21 19:59:34 +0100

    Added code to inflate the three TextViews

--- UserProfile/app/src/main/java/com/example/android/userprofile/MainActivity.java
+++ UserProfile/app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.userprofile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Inflate the TextViews in activity_main.xml
+        // 1. Name
+        TextView name = (TextView)findViewById(R.id.name);
+        name.setText("emgperez");
+
+        // 2. Birthday
+        TextView birthday = (TextView)findViewById(R.id.birthday);
+        birthday.setText("10/20/1982");
+
+        // 3. Country
+        TextView country = (TextView)findViewById(R.id.country);
+        country.setText("Spain");
+
+
+
+
     }
 }
