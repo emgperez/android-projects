2ef22e18d7013713d368f3e6d012f9c61065b28d
emgperez <em.gonzalez.perez@gmail.com>
2018-03-03 11:18:25 +0100

    Updated 'checkAnswers' method

--- IdiomaticGermanQuizApp/app/src/main/java/com/example/android/idiomaticgermanquizapp/MainActivity.java
+++ IdiomaticGermanQuizApp/app/src/main/java/com/example/android/idiomaticgermanquizapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.idiomaticgermanquizapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -43,7 +44,51 @@ public class MainActivity extends AppCompatActivity {
         if (answerTwo.isChecked())
             correctAnswers += 1;
 
+        // Question number three
+        RadioButton answerThree = (RadioButton)findViewById(R.id.choice_3_1);
+        if (answerThree.isChecked())
+            correctAnswers += 1;
 
+        // Question number four
+        RadioButton answerFour = (RadioButton)findViewById(R.id.choice_4_1);
+        if (answerFour.isChecked())
+            correctAnswers += 1;
+
+        // Question number five
+        RadioButton answerFive = (RadioButton)findViewById(R.id.choice_5_3);
+        if (answerFive.isChecked())
+            correctAnswers += 1;
+
+        // Question number six
+        RadioButton answerSix = (RadioButton)findViewById(R.id.choice_6_4);
+        if (answerSix.isChecked())
+            correctAnswers += 1;
+
+        // Question number seven
+        RadioButton answerSeven = (RadioButton)findViewById(R.id.choice_7_1);
+        if (answerSeven.isChecked())
+            correctAnswers += 1;
+
+        // Question number eight
+        RadioButton answerEight = (RadioButton)findViewById(R.id.choice_8_4);
+        if (answerEight.isChecked())
+            correctAnswers += 1;
+
+        // Question number nine
+        RadioButton answerNine = (RadioButton)findViewById(R.id.choice_9_2);
+        if (answerNine.isChecked())
+            correctAnswers += 1;
+
+        // Question number ten
+        RadioButton answerTen = (RadioButton)findViewById(R.id.choice_10_2);
+        if (answerTen.isChecked())
+            correctAnswers += 1;
+
+        // Question number eleven (two answers needed)
+        CheckBox answerEleven_1 = (CheckBox)findViewById(R.id.choice_11_2);
+        CheckBox answerEleven_2 = (CheckBox)findViewById(R.id.choice_11_3);
+        if (answerEleven_1.isChecked() && answerEleven_2.isChecked())
+            correctAnswers += 1;
 
     }
 
