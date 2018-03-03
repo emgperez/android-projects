2cde6e4030e8e4ddaab4730c5476dbca59fcd470
emgperez <em.gonzalez.perez@gmail.com>
2018-03-03 10:53:10 +0100

    Added method to submit quiz questions

--- IdiomaticGermanQuizApp/app/src/main/java/com/example/android/idiomaticgermanquizapp/MainActivity.java
+++ IdiomaticGermanQuizApp/app/src/main/java/com/example/android/idiomaticgermanquizapp/MainActivity.java
@@ -2,8 +2,30 @@ package com.example.android.idiomaticgermanquizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Number of correct answers
+    int correctAnswers = 0;
+
+    /**
+     * This methods triggers the checking of your quiz answers
+     * @param view
+     */
+    public void submitQuestions(View view) {
+
+        checkAnswers(view);
+
+        Toast.makeText(this ,"Correct Answers: " + correctAnswers, Toast.LENGTH_SHORT).show();
+        resetAnswers();
+
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
