d7d06c866e3e829575db6626b2dc7c1743b5563b
emgperez <em.gonzalez.perez@gmail.com>
2018-03-06 22:05:54 +0100

    Cleaned the code

--- IdiomaticGermanQuizApp/app/src/main/java/com/example/android/idiomaticgermanquizapp/MainActivity.java
+++ IdiomaticGermanQuizApp/app/src/main/java/com/example/android/idiomaticgermanquizapp/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     // Number of correct answers
     int correctAnswers;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
