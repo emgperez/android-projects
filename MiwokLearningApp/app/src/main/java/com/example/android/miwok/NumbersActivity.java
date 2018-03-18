bbcc37a3d4ccaf0c4b764f65c8d894472ceac236
emgperez <em.gonzalez.perez@gmail.com>
2018-03-18 18:40:18 +0100

    Added Views for the second and third element in Numbers

--- MiwokLearningApp/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ MiwokLearningApp/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -31,10 +31,23 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("ten");
 
         LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+        // Create view for the first element in the array
         TextView wordView = new TextView(this);
         wordView.setText(words.get(0));
-
         // Add child view to parent
         rootView.addView(wordView);
+
+        // Create view for the second element in the array
+        TextView wordView2 = new TextView(this);
+        wordView2.setText(words.get(1));
+        // Add child view to parent
+        rootView.addView(wordView2);
+
+        // Create view for the third element in the array
+        TextView wordView3 = new TextView(this);
+        wordView3.setText(words.get(2));
+        // Add child view to parent
+        rootView.addView(wordView3);
     }
 }
