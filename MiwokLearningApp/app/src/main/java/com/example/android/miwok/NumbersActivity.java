32d0806044c7c02aa24370754d5d5cef623aadcb
emgperez <em.gonzalez.perez@gmail.com>
2018-03-18 20:32:58 +0100

    Added while loop to display elements in the ArrayList

--- MiwokLearningApp/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ MiwokLearningApp/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -32,26 +32,21 @@ public class NumbersActivity extends AppCompatActivity {
 
         LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
 
-        // TODO: replace this code with a loop
+        // Index to control the loop
+        int wordIndex = 0;
 
-        // Create view for the first element in the array
-        TextView wordView = new TextView(this);
-        wordView.setText(words.get(0));
-        // Add child view to parent
-        rootView.addView(wordView);
+        while(wordIndex < words.size())
+        {
+            // Create view for the element in the array
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(wordIndex));
 
-        // Create view for the second element in the array
-        TextView wordView2 = new TextView(this);
-        wordView2.setText(words.get(1));
-        // Add child view to parent
-        rootView.addView(wordView2);
+            // Add child view to parent
+            rootView.addView(wordView);
 
-        // Create view for the third element in the array
-        TextView wordView3 = new TextView(this);
-        wordView3.setText(words.get(2));
-        // Add child view to parent
-        rootView.addView(wordView3);
-
+            // Update index to keep on looping
+            wordIndex++;
+        }
 
     }
 }
