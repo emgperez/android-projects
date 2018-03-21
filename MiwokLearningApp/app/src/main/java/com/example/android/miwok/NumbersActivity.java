7da8bc4a1099b974612332eb596292b828440c77
emgperez <em.gonzalez.perez@gmail.com>
2018-03-21 20:50:28 +0100

    Removed the for loop and added an ArrayAdapter

--- MiwokLearningApp/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ MiwokLearningApp/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -30,20 +32,14 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        // ArrayAdapter to create a View for each element in the words list
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
-        // Index to control the loop
-        int wordIndex;
+        // Find the ListView identified by 'list' in the layout
+        ListView listView = (ListView) findViewById(R.id.list);
 
-        for(wordIndex = 0; wordIndex < words.size(); wordIndex++)
-        {
-            // Create view for the element in the array
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(wordIndex));
-
-            // Add child view to parent
-            rootView.addView(wordView);
-        }
+        // And set the adapter created before
+        listView.setAdapter(itemsAdapter);
 
     }
 }
