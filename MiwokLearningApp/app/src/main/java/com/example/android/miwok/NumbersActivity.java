e8a6f332fcb20986b431c6dc58f6837dee960dc5
emgperez <em.gonzalez.perez@gmail.com>
2018-03-22 20:15:03 +0100

    Tried with a GridView instead of a ListView

--- MiwokLearningApp/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ MiwokLearningApp/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -32,14 +33,17 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        // ArrayAdapter to create a View for each element in the words list
+        // ArrayAdapter to create a View for each element in the words list. Every item in the words list will have
+        // a View in the 'listView' object
         ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
 
         // Find the ListView identified by 'list' in the layout
-        ListView listView = (ListView) findViewById(R.id.list);
+        //ListView listView = (ListView) findViewById(R.id.list);
+        GridView gridView = (GridView)findViewById(R.id.list);
 
         // And set the adapter created before
-        listView.setAdapter(itemsAdapter);
+        //listView.setAdapter(itemsAdapter);
+        gridView.setAdapter(itemsAdapter);
 
     }
 }
