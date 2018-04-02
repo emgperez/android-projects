9f4929a9abca4810b7db16ac96e002e8115eeefe
emgperez <em.gonzalez.perez@gmail.com>
2018-04-02 18:30:42 +0200

    Added Adapter for Playlist list of objects

---
+++ MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/PlaylistAdapter.java
@@ -0,0 +1,62 @@
+package com.example.android.musicalstructureapp;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by emilio on 02/04/2018.
+ */
+
+public class PlaylistAdapter extends ArrayAdapter<Playlist>{
+
+
+
+    public PlaylistAdapter(Activity context, ArrayList<Playlist> lists) {
+
+        // Initialize the adapter internal storage
+        super(context, 0, lists);
+
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the playlist object located at this position in the list
+        Playlist currentPlaylist = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the playlist name text view
+        TextView playlistName = (TextView) listItemView.findViewById(R.id.playlist_name_text_view);
+        playlistName.setText(currentPlaylist.getName());
+
+        // Find the TextView in the list_item.xml layout with the playlist play mode text view
+        TextView playlistPlayMode = (TextView) listItemView.findViewById(R.id.playlist_mode_text_view);
+
+        // Get the play mode for the current playlist
+        playlistPlayMode.setText(currentPlaylist.getPlayMode());
+
+
+        // Return the whole list item layout (containing 2 TextViews and an ImageView)
+        // so that it can be shown in the ListView
+        return listItemView;
+    }
+}
