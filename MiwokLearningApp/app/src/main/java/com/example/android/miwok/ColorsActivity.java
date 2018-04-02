8d9df9de1a72931319809234cb4376ddf35ebbab
emgperez <em.gonzalez.perez@gmail.com>
2018-04-02 19:08:36 +0200

    Added colors translations

--- MiwokLearningApp/app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ MiwokLearningApp/app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        // ArrayList of words
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        // Populate the ArrayList
+        //words.add("one");
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+       
+        // ArrayAdapter to create a View for each element in the words list. Every item in the words list will have
+        // a View in the 'listView' object
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        // Find the ListView identified by 'list' in the layout
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // And set the adapter created before
+        listView.setAdapter(adapter);
     }
 }
