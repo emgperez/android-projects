5282ef51a1c6fb3dabc295c8f5bee906fcfab9a1
emgperez <em.gonzalez.perez@gmail.com>
2018-04-02 18:31:45 +0200

    Added code to populate the playlists view

---
+++ MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/PlaylistsActivity.java
@@ -0,0 +1,50 @@
+package com.example.android.musicalstructureapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+/**
+ * Created by emilio on 02/04/2018.
+ */
+
+public class PlaylistsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        Toast.makeText(this , "OnCreate Playlist", Toast.LENGTH_SHORT).show();
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlists);
+
+        // ArrayList of words
+        ArrayList<Playlist> pLists = new ArrayList<Playlist>();
+
+        // Populate the ArrayList
+        pLists.add(new Playlist("Weird music", "Repeat"));
+        //pLists.add(new Playlist("Uplifting music", "Shuffle"));
+        //pLists.add(new Playlist("Italo Disco", "Repeat"));
+        //pLists.add(new Playlist("Spanish Copla", "Repeat"));
+
+        // ArrayAdapter to create a View for each element in the playlists list. Every item in the list will have
+        // a View in the 'listView' object
+        PlaylistAdapter adapter = new PlaylistAdapter(this, pLists);
+
+        // Find the ListView identified by 'list' in the layout
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // And set the adapter created before
+        listView.setAdapter(adapter);
+
+    }
+}
