61d7cdb730bb26cb82badc287fc0ee35ff9a365f
emgperez <em.gonzalez.perez@gmail.com>
2018-04-02 19:13:59 +0200

    Added family members translations

--- MiwokLearningApp/app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ MiwokLearningApp/app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -2,6 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+
+        // ArrayList of words
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        // Populate the ArrayList
+        //words.add("one");
+        words.add(new Word("father", "әpә"));
+        words.add(new Word("mother", "әṭa"));
+        words.add(new Word("son", "angsi"));
+        words.add(new Word("daughter", "tune"));
+        words.add(new Word("older brother", "taachi"));
+        words.add(new Word("younger brother", "chalitti"));
+        words.add(new Word("older sister", "teṭe"));
+        words.add(new Word("younger sister", "kolliti"));
+        words.add(new Word("grandmother", "ama"));
+        words.add(new Word("grandfather", "paapa"));
+
+        // ArrayAdapter to create a View for each element in the words list. Every item in the words list will have
+        // a View in the 'listView' object
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        // Find the ListView identified by 'list' in the layout
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // And set the adapter created before
+        listView.setAdapter(adapter);
     }
 }
