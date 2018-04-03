4fb915d30dac94e6c5a0cca328de6af9d9d98339
emgperez <em.gonzalez.perez@gmail.com>
2018-04-03 19:43:33 +0200

    Cleaned some lines

--- MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/PlaylistAdapter.java
+++ MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/PlaylistAdapter.java
@@ -7,9 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -38,17 +36,17 @@ public class PlaylistAdapter extends ArrayAdapter<Playlist>{
         View listItemView = convertView;
         if(listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.list_item, parent, false);
+                    R.layout.playlist_item, parent, false);
         }
 
         // Get the playlist object located at this position in the list
         Playlist currentPlaylist = getItem(position);
 
-        // Find the TextView in the list_item.xml layout with the playlist name text view
+        // Find the TextView in the playlist_item.xml.xml layout with the playlist name text view
         TextView playlistName = (TextView) listItemView.findViewById(R.id.playlist_name_text_view);
         playlistName.setText(currentPlaylist.getName());
 
-        // Find the TextView in the list_item.xml layout with the playlist play mode text view
+        // Find the TextView in the playlist_itemitem.xml layout with the playlist play mode text view
         TextView playlistPlayMode = (TextView) listItemView.findViewById(R.id.playlist_mode_text_view);
 
         // Get the play mode for the current playlist
