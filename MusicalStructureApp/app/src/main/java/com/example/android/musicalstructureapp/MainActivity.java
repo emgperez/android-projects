4effd05c2b4792dcc0281371c90fb2553ab6242e
emgperez <em.gonzalez.perez@gmail.com>
2018-04-04 19:52:50 +0200

    Cleaned the code

--- MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
+++ MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
@@ -12,17 +12,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //Toast.makeText(this , "OnCreate MainActivity 1", Toast.LENGTH_SHORT).show();
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Toast.makeText(this , "OnCreate MainActivity 2", Toast.LENGTH_SHORT).show();
-
         // Find the view that shows the playlist category
         TextView pLists = (TextView)findViewById(R.id.playLists);
-
-        //Toast.makeText(this , "Found TextView list", Toast.LENGTH_SHORT).show();
 
         // Set a click listener on that View
         pLists.setOnClickListener(new View.OnClickListener(){
@@ -40,8 +34,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Find the view that shows the song category
         TextView pSongs = (TextView)findViewById(R.id.songs);
-
-        //Toast.makeText(this , "Found TextView list", Toast.LENGTH_SHORT).show();
 
         // Set a click listener on that View
         pSongs.setOnClickListener(new View.OnClickListener(){
@@ -61,8 +53,6 @@ public class MainActivity extends AppCompatActivity {
         // Find the view that shows the artist category
         TextView pArtists = (TextView)findViewById(R.id.artists);
 
-        //Toast.makeText(this , "Found TextView list", Toast.LENGTH_SHORT).show();
-
         // Set a click listener on that View
         pArtists.setOnClickListener(new View.OnClickListener(){
 
@@ -77,5 +67,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 }
