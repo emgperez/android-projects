833065ea35fe7bd993d8552874e5b645a68c4a23
emgperez <em.gonzalez.perez@gmail.com>
2018-04-04 19:05:09 +0200

    Cleaned the code

--- MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/SongAdapter.java
+++ MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/SongAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.musicalstructureapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -8,8 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 import java.util.ArrayList;
 
@@ -26,8 +29,6 @@ public class SongAdapter extends ArrayAdapter<Song>{
 
         // Initialize the adapter internal storage
         super(context, 0, songs);
-
-
     }
 
     @NonNull
