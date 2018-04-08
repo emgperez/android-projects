b1a39285981a747ca781d6b79d952aa5a63fd0d9
emgperez <em.gonzalez.perez@gmail.com>
2018-04-08 09:30:01 +0200

    Changed drawables and modified image resources to point to them

--- MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/SongsActivity.java
+++ MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/SongsActivity.java
@@ -27,14 +27,16 @@ public class SongsActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songs);
 
+        String[] values = new String[]{"Playlists"};
+
         // ArrayList of words
         ArrayList<Song> songList = new ArrayList<Song>();
 
         // Populate the ArrayList
-        songList.add(new Song(getString(R.string.song_one), getString(R.string.artist_one), getString(R.string.album_one), R.mipmap.ic_queen));
-        songList.add(new Song(getString(R.string.song_two), getString(R.string.artist_two), getString(R.string.album_two), R.mipmap.ic_bruno_mars));
-        songList.add(new Song(getString(R.string.song_three), getString(R.string.artist_three), getString(R.string.album_three), R.mipmap.ic_jacko));
-        songList.add(new Song(getString(R.string.song_four), getString(R.string.artist_four), getString(R.string.album_four), R.mipmap.ic_adele));
+        songList.add(new Song(getString(R.string.song_one), getString(R.string.artist_one), getString(R.string.album_one), R.drawable.ic_queen));
+        songList.add(new Song(getString(R.string.song_two), getString(R.string.artist_two), getString(R.string.album_two), R.drawable.ic_bruno_mars));
+        songList.add(new Song(getString(R.string.song_three), getString(R.string.artist_three), getString(R.string.album_three), R.drawable.ic_jacko));
+        songList.add(new Song(getString(R.string.song_four), getString(R.string.artist_four), getString(R.string.album_four), R.drawable.ic_adele));
 
         // ArrayAdapter to create a View for each element in the songs list. Every item in the list will have
         // a View in the 'listView' object
@@ -45,6 +47,8 @@ public class SongsActivity extends AppCompatActivity{
 
         // And set the adapter created before
         listView.setAdapter(adapter);
+
+
 
     }
 }
