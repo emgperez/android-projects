a9fe9e5f2c05b58e637eb0bf19b5100c8d03b2f6
emgperez <em.gonzalez.perez@gmail.com>
2018-04-14 15:28:29 +0200

    Changed layout for content view

--- MiwokLearningApp/app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ MiwokLearningApp/app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -11,7 +11,7 @@ public class FamilyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.words_list);
+        setContentView(R.layout.word_list);
 
         // ArrayList of words
         ArrayList<Word> words = new ArrayList<Word>();
