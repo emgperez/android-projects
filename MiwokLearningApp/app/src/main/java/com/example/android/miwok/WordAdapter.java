0fb3887a0b2b9bc6341ae1ca59cb2677eb1bffde
emgperez <em.gonzalez.perez@gmail.com>
2018-04-17 20:07:46 +0200

    Added call to set image resource on the Word object

--- MiwokLearningApp/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ MiwokLearningApp/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -58,7 +58,7 @@ public class WordAdapter extends ArrayAdapter<Word>{
 
         // Get the image resource ID from the current Word object and
         // set the image to iconView
-        //iconView.setImageResource(currentAndroidFlavor.getImageResourceId());
+        iconView.setImageResource(currentWord.getImageResourceId());
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
