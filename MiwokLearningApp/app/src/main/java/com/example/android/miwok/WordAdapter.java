d025cb130337438060191e2882f648516501db2e
emgperez <em.gonzalez.perez@gmail.com>
2018-04-17 19:59:41 +0200

    Changed image resource declaration

--- MiwokLearningApp/app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ MiwokLearningApp/app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -54,7 +54,7 @@ public class WordAdapter extends ArrayAdapter<Word>{
         miwokTranslationTextView.setText(currentWord.getMiwokTranslation());
 
         // Find the ImageView in the list_item.xml layout with the ID list_item_icon
-        //ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+        ImageView iconView = (ImageView) listItemView.findViewById(R.id.image);
 
         // Get the image resource ID from the current Word object and
         // set the image to iconView
