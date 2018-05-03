0847ceaf2837b9b0604b37369ba5609b60d2f400
emgperez <em.gonzalez.perez@gmail.com>
2018-05-03 20:06:46 +0200

    Optimized imports

--- MadridTourGuide/app/src/main/java/com/example/android/madridtourguide/WestFragment.java
+++ MadridTourGuide/app/src/main/java/com/example/android/madridtourguide/WestFragment.java
@@ -1,17 +1,12 @@
 package com.example.android.madridtourguide;
 
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
