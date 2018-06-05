9a6c80c9b14122f6a834ff326aa9c7de993ae8ab
emgperez <em.gonzalez.perez@gmail.com>
2018-06-05 20:06:34 +0200

    Implemented main activity

--- NewsAppv1/app/src/main/java/com/example/android/newsappv1/NewsActivity.java
+++ NewsAppv1/app/src/main/java/com/example/android/newsappv1/NewsActivity.java
@@ -1,13 +1,142 @@
 package com.example.android.newsappv1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
+import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class NewsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>>  {
+
+    public static final String LOG_TAG = NewsActivity.class.getName();
+
+    /**
+     * Constant value for the news loader ID.
+     */
+    private static final int NEWS_LOADER_ID = 1;
+
+    /** Adapter for the list of news */
+    private NewsAdapter mAdapter;
+
+    /** URL for news data */
+    private static final String GUARDIANAPIS_REQUEST_URL = "https://content.guardianapis.com/search?q=debate&tag=sport/sport&from-date=2015-04-09&api-key=test&show-tags=contributor";
+
+    /** Empty text view in case there's no item to show*/
+    private TextView emptyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        // Find a reference to the {@link ListView} in the layout
+        ListView newsListView = (ListView) findViewById(R.id.list);
+
+        emptyTextView = (TextView) findViewById(R.id.empty_view);
+        newsListView.setEmptyView(emptyTextView);
+
+        // Create a new adapter that takes an empty list of news as input
+        mAdapter = new NewsAdapter(this, new ArrayList<News>());
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        newsListView.setAdapter(mAdapter);
+
+        // Set an item click listener on the ListView, which sends an intent to a web browser
+        // to open a website with more information about the selected news.
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current news that was clicked on
+                News currentNews = mAdapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri newsUri = Uri.parse(currentNews.getUrl());
+
+                // Create a new intent to view the news URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
+
+        // Check network conectivity with a ConnectivityManager reference
+        ConnectivityManager connMgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // And now get the details of the current network connection
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // We have connection, so get the data
+        if (networkInfo != null && networkInfo.isConnected()) {
+
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+        }
+        else {
+            // No connection, so display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            emptyTextView.setText(R.string.no_internet_connection);
+        }
     }
+
+    @Override
+    public Loader<List<News>> onCreateLoader(int i, Bundle bundle) {
+
+        // Loader for the given URL
+        return new NewsLoader(this, GUARDIANAPIS_REQUEST_URL);
+
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<News>> loader, List<News> earthquakes)
+    {
+        // Clear the adapter of previous earthquake data
+        // mAdapter.clear();
+
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        emptyTextView.setText(R.string.no_news_available);
+
+        // If there's Earthquake data, add it all to the adapter
+        if (earthquakes != null && !earthquakes.isEmpty()) {
+            mAdapter.addAll(earthquakes);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<News>> loader) {
+
+        // Clear existing data
+        mAdapter.clear();
+    }
+
 }
