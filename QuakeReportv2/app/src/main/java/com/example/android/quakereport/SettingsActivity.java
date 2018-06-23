8f2f3a4727639d209094640a37f0311dd9d6159d
emgperez <em.gonzalez.perez@gmail.com>
2018-06-23 18:56:15 +0200

    Added the fragment and the loader to work with the list of preferences

--- QuakeReportv2/app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ QuakeReportv2/app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -21,6 +21,7 @@ import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.preference.ListPreference;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -40,13 +41,27 @@ public class SettingsActivity extends AppCompatActivity {
 
             Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
             bindPreferenceSummaryToValue(minMagnitude);
+
+            Preference orderBy = findPreference(getString(R.string.settings_order_by_key));
+            bindPreferenceSummaryToValue(orderBy);
         }
 
         @Override
         public boolean onPreferenceChange(Preference preference, Object value) {
             // The code in this method takes care of updating the displayed preference summary after it has been changed
             String stringValue = value.toString();
-            preference.setSummary(stringValue);
+
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(stringValue);
+                if (prefIndex >= 0) {
+                    CharSequence[] labels = listPreference.getEntries();
+                    preference.setSummary(labels[prefIndex]);
+                }
+            }
+            else {
+                preference.setSummary(stringValue);
+            }
 
             return true;
         }
@@ -54,7 +69,7 @@ public class SettingsActivity extends AppCompatActivity {
         private void bindPreferenceSummaryToValue(Preference preference) {
             preference.setOnPreferenceChangeListener(this);
 
-            // Get the default value or the value set by the user and put it on the 
+            // Get the default value or the value set by the user and display it
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
             String preferenceString = preferences.getString(preference.getKey(), "");
             onPreferenceChange(preference, preferenceString);
